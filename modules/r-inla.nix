6b3312b5c0cb81b4ac387fd129ccc1b37eb037a9
loganstundal <logan.stundal@gmail.com>
2023-06-20 09:55:43 -0400

    added initial r-inla script but requires more work

---
+++ modules/r-inla.nix
@@ -0,0 +1,32 @@
+
+# This may offer a solution to install r-inla
+# code example from:
+# https://discourse.nixos.org/t/installing-an-archive-tar-gz-zip-etc-to-a-fixed-location-on-nixos/28333/2
+
+{ stdenv
+, lib
+, fetchzip
+}:
+
+stdenv.mkDerivation rec {
+  pname = "vuetorrent";
+  version = "1.5.7";
+
+  src = fetchzip {
+    url = "https://github.com/WDaan/VueTorrent/releases/download/v${version}/vuetorrent.zip";
+    sha256 = "5REe5wtYnmkPnP5wroRbHbiEAqYXx8OHht0O79eVBjY=";
+  };
+
+  buildPhase = "";
+  installPhase = ''
+    mkdir -p $out
+    mv public $out
+  '';
+}
+
+
+# -----
+
+# OR try using nix built-ins to install:
+# https://nixos.org/manual/nix/stable/language/builtins.html?highlight=fetchtarball
+# or fetchurl
